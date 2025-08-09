180b89149be6b390596a9cdb9835a2e9f2cff2cb
FirePenguin99 <gabriellewington@gmail.com>
2025-08-09 21:06:47 +0100

    feat: added fire shield for slow movement

--- Assets/Player/Classes/ShootBehaviour.cs
+++ Assets/Player/Classes/ShootBehaviour.cs
@@ -26,13 +26,15 @@ public class ShootBehaviour : MonoBehaviour
         Debug.DrawRay(transform.position, aimDirection, Color.red, 0.1f);
 
         GameObject currentBullet = Instantiate(bulletPrefab, shootPoint.position, Quaternion.LookRotation(aimDirection));
-        currentBullet.GetComponent<Rigidbody>().AddForce(currentBullet.transform.forward * shootForce, ForceMode.Impulse);
+        Rigidbody bulletRb = currentBullet.GetComponent<Rigidbody>();
+        if (bulletRb) bulletRb.AddForce(currentBullet.transform.forward * shootForce, ForceMode.Impulse);
     }
     public virtual void Fire(float damage)
     {
         Vector3 aimDirection = straightAimDirection.CalculateAimDirection() - shootPoint.position;
         GameObject currentBullet = Instantiate(bulletPrefab, shootPoint.position, Quaternion.LookRotation(aimDirection));
-        currentBullet.GetComponent<Rigidbody>().AddForce(currentBullet.transform.forward * shootForce, ForceMode.Impulse);
+        Rigidbody bulletRb = currentBullet.GetComponent<Rigidbody>();
+        if (bulletRb) bulletRb.AddForce(currentBullet.transform.forward * shootForce, ForceMode.Impulse);
 
         if (damage != 0)
         {
